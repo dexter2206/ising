d0d8f097208802e3a06b7c846f7792fdd68b2d91
Konrad Jałowiecki <dexter2206@gmail.com>
2019-01-13 20:03:40 +0100

    Fix exporter paths

--- travisci/install-cuda-trusty.sh
+++ travisci/install-cuda-trusty.sh
@@ -6,6 +6,6 @@ travis_retry wget --progress=dot:mega https://developer.nvidia.com/compute/cuda/
 chmod +x cuda_*_linux
 sudo ./cuda_*_linux --silent --toolkit
 
-export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
+export CUDA_HOME=/usr/local/cuda-9.2
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
 export PATH=${CUDA_HOME}/bin:${PATH}
