563b350ed517c0810b253d8c03db8f6980b4688f
Konrad Jałowiecki <dexter2206@gmail.com>
2019-01-14 09:20:13 +0100

    Add script for building wheels

---
+++ travisci/build-wheels.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -x
+
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install numpy
+    CPATH=$CPATH:/thrust "${PYBIN}/pip" wheel /ising/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /ising/wheelhouse/
+done
+
+# Install packages and test
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install ising --no-index -f /ising/wheelhouse
+done
