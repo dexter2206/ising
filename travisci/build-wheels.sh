84944ee754c49afc912fc2e78cc5dabff685c240
Konrad Jałowiecki <dexter2206@gmail.com>
2019-01-14 10:31:43 +0100

    Run auditwheel only for ising wheels

--- travisci/build-wheels.sh
+++ travisci/build-wheels.sh
@@ -9,7 +9,7 @@ for PYBIN in /opt/python/*/bin; do
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in wheelhouse/ising*manylinux*.whl; do
     auditwheel repair "$whl" -w /ising/wheelhouse/
 done
 
