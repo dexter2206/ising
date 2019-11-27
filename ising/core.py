c9f2987826b097661d60171570351bf5c0d2e202
Konrad Jałowiecki <dexter2206@gmail.com>
2019-11-27 13:20:24 +0100

    Update core to use C++ implementation

--- ising/core.py
+++ ising/core.py
@@ -5,133 +5,92 @@ import progressbar
 import psutil
 from ising.helpers import EnergiesAndStates, decode_state, ising_to_qubo
 from ising.graphtools import read_graph
-from isingcpu import cpusearch
+import isingcpu
 
 if not logging.getLogger('ising').handlers:
     logging.basicConfig(level='WARNING')
 
 try:
-    from isinggpu import gpusearch
+    import isinggpu
 except ImportError:
     logging.getLogger('ising').info('The ising package is installed with no GPU support.')
-    gpusearch = None
+    isinggpu = None
 
-def search(graph, num_states=10, energies_only=False, **kwargs):
-    """Find lowest energies and corresponding states of Ising Model specified by Jh.
+def search(
+    graph,
+    num_states=10,
+    energies_only=False,
+    use_gpu=False,
+    block_size=32,
+    chunk_exponent=None,
+    precision="single",
+    show_progress=False
+):
+    if use_gpu and isinggpu is None:
+        raise ValueError(
+            "The ising package has been installed without GPU support. Set use_gpu=False"
+        )
 
-    :param Jh: matrix encoding hamiltonian of the sytem. Should be real and symmetric.
-    :type Jh: numpy.ndarray or array-like
-    :param sweep_size: number of bits being changed during a single sweep during exhaustive
-     search. Has to obey `1 <= sweep_size <= Jh.shape[0]`.
-    :type sweep_size: int
-    :param num_states: how many lowest states to keep. Default is 10.
-    :type num_states:
-    :returns: namedtuple with components `states` and `energies` holding computed states of
-     lowest energy and corresponding energies.
-    :rtype: :py:class:`EnergiesAndStates
-    """
     logger = logging.getLogger('ising')
-
     jh_mat, labels = read_graph(graph)
-
     qubo, const = ising_to_qubo(jh_mat)
-    method = choose_method(gpusearch, **kwargs)
-    logger.info('Choosen computation mehtod: %s', method)
+    kwargs = {}
 
-    if 'chunk_size' in kwargs:
-        chunk_size = kwargs['chunk_size']
-        logger.debug('Chunk size given explicitly as %f.', chunk_size)
+    method = "gpu" if use_gpu else "cpu"
+
+    if chunk_exponent is not None:
+        logger.debug('Chunk size given explicitly as %f.', chunk_exponent)
     else:
         logger.debug('Deducing chunk size from the available memory.')
-        chunk_size = max_chunk_size_for_method(method)
+        chunk_exponent = max_chunk_size_for_method(method)
 
-    if chunk_size > jh_mat.shape[0]:
-        logger.debug('Clipping chunk size to the number of spins.')
-        chunk_size = jh_mat.shape[0]
+    if chunk_exponent > jh_mat.shape[0]:
+        logger.debug('Clipping chunk size exponent to the number of spins.')
+        chunk_exponent = jh_mat.shape[0]
 
-    logger.info('Chunk size exponent that will be used: %d', chunk_size)
+    if use_gpu:
+        kwargs["block_size"] = block_size
+        kwargs["grid_size"] = int(numpy.ceil(2 ** (chunk_exponent) / block_size))
+        module = isinggpu
+    else:
+        module = isingcpu
 
-    if num_states > 2 * 2 ** chunk_size:
+    logger.info('Choosen computation mehtod: %s', method)
+
+    logger.info('Chunk size exponent that will be used: %d', chunk_exponent)
+
+    if num_states > 2 * 2 ** chunk_exponent:
         logger.warning('Requested more states than two chunks. Clipping at 2 ** (chunk size)')
-        num_states = 2 ** chunk_size
+        num_states = 2 ** chunk_exponent
 
-    if kwargs.get('show_progress', False):
-        pbar = progressbar.ProgressBar(max_value=2 ** (jh_mat.shape[0]-chunk_size))
-        callback = pbar.update
-    else:
-        pbar = None
-        callback = dummy_callback
+    dtype = numpy.float32 if precision == "single" else numpy.float64
 
-    if method.lower() == 'cpu':
-        logger.info('Running CPU search: Jh.shape=%s, chunk_size=%d, num_states=%d',
-                    jh_mat.shape, chunk_size, num_states)
-        callback(0)
-        if energies_only:
-            energies = cpusearch.find_lowest_energies_only(qubo,
-                                                           chunk_size,
-                                                           num_states,
-                                                           callback=callback)
-        else:
-            energies, state_reprs = cpusearch.find_lowest(qubo,
-                                                          chunk_size,
-                                                          num_states,
-                                                          callback=callback)
+    kwargs["Q"] = numpy.array(qubo, dtype=dtype)
+    kwargs["show_progress"] = show_progress
+    kwargs["num_spins"] = qubo.shape[0]
+    kwargs["chunk_exponent"] = chunk_exponent
+    kwargs["energies_out"] = numpy.empty(num_states, dtype=dtype)
+    kwargs["num_states"] = num_states
+
+    if precision == "single" and energies_only:
+        find = module.cy_find_lowest_energies_only_float
+    elif precision == "single":
+        find = module.cy_find_lowest_float
+        kwargs["states_out"] = numpy.empty(num_states, dtype=numpy.int64)
+    elif energies_only:
+        find = module.cy_find_lowest_energies_only_double
     else:
-        logger.info('Running GPU search: Jh.shape=%s, num_states=%d, chunk_size=%d',
-                    jh_mat.shape, num_states, chunk_size)
-        callback(0)
-        if energies_only:
-            energies = gpusearch.find_lowest_energies_only(qubo,
-                                                           chunk_size,
-                                                           num_states,
-                                                           callback=callback)
-        else:
-            energies, state_reprs = gpusearch.find_lowest(qubo,
-                                                          chunk_size,
-                                                          num_states,
-                                                          callback=callback)
-    if pbar is not None:
-        pbar.finish()
-    if energies_only:
-        state_reprs = None
-    return EnergiesAndStates(raw_states=state_reprs, labels=labels, energies=energies + const)
+        find = module.cy_find_lowest_double
+        kwargs["states_out"] = numpy.empty(num_states, dtype=numpy.int64)
+
+    find(**kwargs)
+    states_out = None if energies_only else kwargs["states_out"]
+
+    return EnergiesAndStates(raw_states=states_out, labels=labels, energies=kwargs["energies_out"] + const)
 
 def dummy_callback(_):
     """Dummy callback doing nothing."""
     pass
-
-def choose_method(gpusearch, **kwargs):
-    """Choose computation method judging by user supplied args and availability of extensions.
-
-    :param gpusearch: the imported gpusearch Fortran extension, or None if no such extension
-     was available.
-    :type gpusearch: Fortran module or None
-    :param kwargs: keyword arguments passed by the user to the :py:func:`search` function.
-    :type kwargs: dict
-    :returns: String with computation method choosen (either 'cpu' or 'gpu').
-    :rtype: str
-    :raises ValueError: if the 'method' kwarg was supplied but is not equal to 'cpu' or 'gpu'
-    (case insensitive) OR 'gpu' computation method was requested but no GPU extension for
-    the `ising` package has been built and instaled.
-
-    .. notes::
-       The only kwarg supported is the "method" kwarg, which can override automatic selection
-       of computation method.
-    """
-    logger = logging.getLogger('ising')
-    supplied_method = kwargs.get('method')
-    if supplied_method is None:
-        logger.info('No computation method supplied. Determining computation method myself.')
-        return 'gpu' if gpusearch is not None else 'cpu'
-    supplied_method = supplied_method.lower()
-    if supplied_method not in ('cpu', 'gpu'):
-        raise ValueError('Unknown computation method supplied: %s', supplied_method)
-    if supplied_method == 'gpu' and gpusearch is None:
-        raise ValueError('Computation via GPU implementation requested but extension with GPU '
-                         'implementation was not build and installed. If your setup is equipped '
-                         'with CUDA supporting GPU rebuild with --usecuda flag and reinstall '
-                         'the package.')
-    return supplied_method
 
 def determine_max_sweep_size():
     available = psutil.virtual_memory().available
@@ -193,7 +152,7 @@ def max_chunk_size_for_method(method):
     if method == 'cpu':
         mem_bytes = psutil.virtual_memory().available
     elif method == 'gpu':
-        mem_bytes = gpusearch.get_device_properties()
+        mem_bytes, _ = isinggpu.cy_get_gpu_mem_info()
     else:
         raise ValueError('Unknown computation method: %s', method)
     return max_chunk_size(mem_bytes)
