a852c1bd9e64ed255d21d94ab162cdb6343cc5d6
dexter2206 <dexter2206@gmail.com>
2020-04-19 13:37:27 +0200

    Optimize number of partition algorithm calls.

--- examples/simplest.py
+++ examples/simplest.py
@@ -2,8 +2,8 @@
 from ising import search
 
 def main():
-    graph = {(1,1):-1, (1,2):-0.2}
-    solution = search(graph, num_states=4)
+    graph = {(0, 0): -1, (1,1):-1, (2, 2): -1, (3, 3): -1}
+    solution = search(graph, num_states=10, show_progress=True, use_gpu=False)
     print('Energy | State')
     for energy, state in zip(solution.energies, solution.states):
         print('{0:6.2f} | {1}'.format(energy, state))
