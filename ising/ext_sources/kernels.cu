3b85c258f5c02cfaf3c8989e0dab88edf7688ace
dexter2206 <dexter2206@gmail.com>
2020-04-19 17:51:15 +0200

    Apply astyle to all C++/CUDA sources

--- ising/ext_sources/kernels.cu
+++ ising/ext_sources/kernels.cu
@@ -4,49 +4,49 @@ template <typename T>
 __device__
 T energy(int64_t state_repr, T* Q, int N)
 {
-  int i, j;
-  char bits[64];
-  T energy = 0;
+    int i, j;
+    char bits[64];
+    T energy = 0;
 
-  for(i = 0; i < N; i++) {
-      bits[i] = (state_repr >> i) & 1;
-  }
-
-  for(i = 0; i < N; i++) {
-    if(bits[i]) {
-      for(j = i; j < N; j++) {
-	energy -= Q[i * N + j] * bits[i] * bits[j];
-      }
+    for(i = 0; i < N; i++) {
+        bits[i] = (state_repr >> i) & 1;
     }
-  }
-  return energy;
+
+    for(i = 0; i < N; i++) {
+        if(bits[i]) {
+            for(j = i; j < N; j++) {
+                energy -= Q[i * N + j] * bits[i] * bits[j];
+            }
+        }
+    }
+    return energy;
 }
 
 
 template <typename T>
 void __global__ search(T* Q, int N, int sweep_size, T* energies, long int* states, long int m)
 {
-  int idx = threadIdx.x + blockDim.x * blockIdx.x;
-  int i;
-  long int state_repr;
-  if(idx < sweep_size) {
-    state_repr = idx + m;
-    states[idx] = state_repr;
-    energies[idx] = energy(state_repr, Q, N);
-  }
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    int i;
+    long int state_repr;
+    if(idx < sweep_size) {
+        state_repr = idx + m;
+        states[idx] = state_repr;
+        energies[idx] = energy(state_repr, Q, N);
+    }
 }
 
 
 template <typename T>
 void __global__ search_energies_only(T* Q, int N, int sweep_size, T* energies, long int m)
 {
-  int idx = threadIdx.x + blockDim.x * blockIdx.x;
-  int i;
-  long int state_repr;
-  if(idx < sweep_size) {
-    state_repr = idx + m;
-    energies[idx] = energy(state_repr, Q, N);
-  }    
+    int idx = threadIdx.x + blockDim.x * blockIdx.x;
+    int i;
+    long int state_repr;
+    if(idx < sweep_size) {
+        state_repr = idx + m;
+        energies[idx] = energy(state_repr, Q, N);
+    }
 }
 
 
